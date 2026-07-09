40cb45d188ce8568bdd7337554186d8eebde4116
vaidhiii <vaidhiprajapati4221@gmail.com>
2026-07-09 22:06:47 +0530

    Add math module

---
+++ Day4/math_module.py
@@ -0,0 +1,6 @@
+import math
+
+num1 = int(input("Enter number you want square root of:"))
+num2 = int(input("Enter number you want Factorial of:"))
+print("Square root:", math.sqrt(num1))
+print("Factorial :", math.factorial(num2))
